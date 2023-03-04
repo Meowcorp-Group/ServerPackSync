21c1045e26bd1b4d86ee3eb8ed48690c7d919b7d
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-03-04 09:55:26 +0700

    tmp

--- sh/sync-bootstrap.sh
+++ sh/sync-bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-REMOTE_VER_URL="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/version.txt"
-# REMOTE_VER_URL="file://$HOME/Projects/ServerPackSync/db/sync.json"
-REMOTE_VER=$(curl -sSL $REMOTE_VER_URL)
+# REMOTE_VER_URL="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/version.txt"
+REMOTE_VER_URL="file://$HOME/Projects/ServerPackSync/db/sync.json"
+REMOTE_VER=$(curl -sSL $REMOTE_VER_URL | jq -r ".version")
 
 if [ -f "$INST_MC_DIR/sync.json" ] && cat "$INST_MC_DIR/sync.json" | jq -e 'has("version")' >/dev/null 2>&1; then
 	LOCAL_VER=$(cat "$INST_MC_DIR/sync.json" | jq -r ".version")
@@ -15,7 +15,9 @@ fi
 
 echo $LOCAL_VER
 
-REMOTE="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/sh/sync.sh"
-# REMOTE="file://$HOME/Projects/ServerPackSync/sh/sync.sh"
+# REMOTE="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/sh/sync.sh"
+REMOTE="file://$HOME/Projects/ServerPackSync/sh/sync.sh"
 
-curl -sSL $REMOTE | bash -s -- "$@"
+curl -sSL $REMOTE -o sync.sh
+
+bash sync.sh "$@"
