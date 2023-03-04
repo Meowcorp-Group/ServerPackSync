5172c7428bfa4c0c2380adf7a8262e97ce842313
Waoweens <30313089+Waoweens@users.noreply.github.com>
2023-03-04 09:29:49 +0700

    bootstrap - download file instead of running it

--- sh/sync-bootstrap.sh
+++ sh/sync-bootstrap.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+# REMOTE_VER_URL="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/version.txt"
+REMOTE_VER_URL="file://$HOME/Projects/ServerPackSync/db/sync.json"
+REMOTE_VER=$(curl -sSL $REMOTE_VER_URL)
+
+if [ -f "$INST_MC_DIR/sync.json" ] && cat "$INST_MC_DIR/sync.json" | jq -e 'has("version")' >/dev/null 2>&1; then
+	LOCAL_VER=$(cat "$INST_MC_DIR/sync.json" | jq -r ".version")
+else
+	echo "No sync.json found, creating one..."
+	LOCAL_VER="$REMOTE_VER"
+	touch "$INST_MC_DIR/sync.json"
+	echo '{}' | jq ".version = \"$LOCAL_VER\"" > "$INST_MC_DIR/sync.json"
+	echo '{}' | jq ".version = \"$LOCAL_VER\"" > "$INST_MC_DIR/sync.json"
+	echo '{}' | jq ".version = \"$LOCAL_VER\"" > "$INST_MC_DIR/sync.json"
+
+fi
+
+echo $LOCAL_VER
+
 # REMOTE="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/sh/sync.sh"
 REMOTE="file://$HOME/Projects/ServerPackSync/sh/sync.sh"
 
-curl -sSL $REMOTE | bash -s -- "$@"
+# curl -sSL $REMOTE | bash -s -- "$@"
--- sh/sync.sh
+++ sh/sync.sh
@@ -6,20 +6,8 @@
 MODPACK_ID="$1"
 
 # debug
-cd "$INST_DIR"
 
-# Find the modpack directory
-# Some modpacks use `minecraft` and some use `.minecraft`
-if [ -d "$INST_DIR/minecraft" ]; then
-	MODPACK_DIR="$INST_DIR/minecraft"
-elif [ -d "$INST_DIR/.minecraft" ]; then
-	MODPACK_DIR="$INST_DIR/.minecraft"
-else
-	echo "Invalid modpack installation"
-	exit 1
-fi
-
-cd "$MODPACK_DIR"
+cd "$INST_MC_DIR"
 
 # Fetch server pack index
 # REMOTE="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/serverpacks.sh"
@@ -27,6 +15,7 @@ REMOTE="file://$HOME/Projects/ServerPackSync/db/serverpacks.json"
 INDEX=$(curl -sSL $REMOTE)
 
 MODS=$(echo "$INDEX" | jq -r ".[\"${MODPACK_ID}\"].extraMods")
+echo "$MODS"
 
 # debug
 exit 255
