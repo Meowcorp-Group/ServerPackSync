afc08c95a994544a6351b7d454d4e07ab282037f
30313089+Waoweens@users.noreply.github.com <30313089+Waoweens@users.noreply.github.com>
2023-03-06 06:43:51 +0700

    quick

--- sh/sync-bootstrap.sh
+++ sh/sync-bootstrap.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 # ServerPackSync bootstrap
 
-if [ $SYNC_DEBUG = "true" ]; then
+if [ "$SYNC_DEBUG" = "true" ]; then
 	echo -e "\033[1;33mDebug mode enabled\033[0m" >&2
 	remote_ver_url="http://127.0.0.1:7171/db/sync.json"
-	remote="http://http://127.0.0.1:7171/sh/sync.sh"
+	remote="http://127.0.0.1:7171/sh/sync.sh"
 else
 	remote_ver_url="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/sync.json"
 	remote="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/sh/sync.sh"
 fi
+
+remote_ver=$(curl -sSL "$remote_ver_url" | jq -r ".version")
 
 tover() {
     version="$1"
     echo "${version:0:1}.${version:1:1}.${version:2:1}"
 }
 
-# Get remote version number
-remote_ver=$(curl -sSL "$remote_ver_url" | jq -r ".version")
-
 # Check if sync.json file exists and has a version number
-if [ -f "$INST_MC_DIR/sync.json" ] && cat "$INST_MC_DIR/sync.json" | jq 'has("version")' >/dev/null 2>&1; then
+if [ -f "$INST_MC_DIR/sync.json" ] && [ -n "$(cat "$INST_MC_DIR/sync.json" | jq 'has("version")')" ]; then
     local_ver=$(cat "$INST_MC_DIR/sync.json" | jq -r ".version")
 else
     echo "No sync.json found, creating one..."
@@ -35,6 +34,9 @@ if [ "$local_ver" != "$remote_ver" ]; then
     curl -#SL "$remote" -o "$INST_MC_DIR/sync.sh"
     sync_json=$(cat "$INST_MC_DIR/sync.json")
     echo "$sync_json" | jq ".version = \"$remote_ver\" | .version |= tonumber" >"$INST_MC_DIR/sync.json"
+elif [ "$SYNC_DEBUG" = "true" ]; then
+	# download anyways
+	curl -#SL "$remote" -o "$INST_MC_DIR/sync.sh"
 fi
 
 echo "ServerPackSync $(tover "$local_ver")"
--- sh/sync.sh
+++ sh/sync.sh
@@ -1,19 +1,58 @@
 #!/usr/bin/env bash
+# ServerPackSync
+
+cd "$INST_MC_DIR"
+
+if [ "$SYNC_DEBUG" = "true" ]; then
+	remote="http://127.0.0.1:7171/db/serverpacks.json"
+else
+	remote="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/serverpacks.json"
+fi
 
 # Modpack ID
 # Format: <Modpack Host>:<Modpack ID>
 # Example: CF:520914
-MODPACK_ID="$1"
+modpackID="$1"
 
-cd "$INST_MC_DIR"
+# index format
+# [
+#   {
+#     "name": "Emojiful",
+#     "host": "CurseForge",
+#     "fileName": "Emojiful-Forge-1.19.2-4.0.4-all.jar",
+#     "projectID": "284324",
+#     "fileID": "4326651"
+#   },
+#   ...
+# ]
+indexRemote=$(curl -sSL "$remote" | jq -r ".[\"${modpackID}\"].extraMods")
 
-# Fetch server pack index
-# REMOTE="https://raw.githubusercontent.com/Meowcorp-Group/ServerPackSync/main/db/serverpacks.sh"
-REMOTE="file://$HOME/Projects/ServerPackSync/db/serverpacks.json"
-INDEX=$(curl -sSL $REMOTE)
+if [ -n "$(cat "$INST_MC_DIR/sync.json" | jq 'has("mods")')" ]; then
+	indexLocal=$(cat "$INST_MC_DIR/sync.json" | jq -r ".mods")
+else
+	indexLocal="$indexRemote"
+	syncJson=$(cat "$INST_MC_DIR/sync.json")
+	echo "$syncJson" | jq ".[\"mods\"] = $indexRemote" >"$INST_MC_DIR/sync.json"
+fi
 
-MODS=$(echo "$INDEX" | jq -r ".[\"${MODPACK_ID}\"].extraMods")
-# echo "$MODS"
+# compare local and remote indexes
+# and see which mods have different fileIDs
 
-# debug
-exit 255
+
+# loop over curseforge mods
+# https://edge.forgecdn.net/files/XXXX/XXX/filename.jar
+# XXXX/XXX is just the fileID with a slash after the first 4 digits
+# for mod in $(echo "$mods" | jq -r ".[] | select(.host == \"CurseForge\") | .fileID"); do
+# 	fileID=$(echo "$mod" | cut -c 1-4)/$(echo "$mod" | cut -c 5-)
+# 	fileName=$(echo "$mods" | jq -r ".[] | select(.fileID == \"$mod\") | .fileName")
+# 	# if file already exists, check if it's the same
+# 	if [ ! -f "mods/$fileName" ]; then
+# 		echo "Downloading $fileName"
+# 		curl -#SL "https://edge.forgecdn.net/files/$fileID/$fileName" -o "mods/$fileName"
+# 	else
+# 		echo "$fileName already exists, skipping"
+# 	fi
+# done
+
+# TODO: loop over modrinth mods
+# thank you modrinth for having an open api
